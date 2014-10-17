c00387e110d9aea87c17d9e41469931a8f4f491c
40904@longroad.ac.uk <40904@PC004152.coll.lr.local>
2014-10-17 14:12:03 +0100

    Iteration class exercise 2

---
+++ Iteration class exercise 2.py
@@ -0,0 +1,10 @@
+#Kavilan Naidoo
+#17-10-2014
+#Iteration class exercise 2
+
+message=input("please enter a message you would like to display: ")
+times=int(input("please enter the amount of times you want it to be repeated: "))
+
+for count in range(times):
+    print(message)
+    
---
+++ Message amount of time.md
@@ -0,0 +1,9 @@
+```
+OUPUT "Please enter a message: "
+INPUT message
+OUTPUT "please enter the amount of times you want it to be repeated: "
+INPUT times
+
+FOR count <- times
+    OUPUT "message"
+```
