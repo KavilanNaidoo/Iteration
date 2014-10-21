cbf986777e97a4c5e4defc0321dc2b82bcda764e
40904@longroad.ac.uk <40904@PC004152.coll.lr.local>
2014-10-21 12:38:19 +0100

    Iteration

---
+++ Iteration class exercise 4.py
@@ -0,0 +1,12 @@
+  #Kavilan Naidoo
+#17-10-2014
+#iteration class exercise 4
+
+number = 0
+while not 21> number >9:
+    number = int(input("Please enter a number: "))
+    if not 21>number>9:
+        print("Invalid")
+print("Valid")
+     
+     
